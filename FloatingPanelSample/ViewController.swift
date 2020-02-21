62f98d4c0a29a6150689e5a41b5f5d803e7374f1
kohei <kohei.saito@uzabase.com>
2020-02-21 12:15:42 +0900

    Initial Commit

---
+++ FloatingPanelSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FloatingPanelSample
+//
+//  Created by kohei.saito on 2020/02/21.
+//  Copyright © 2020 kohei.saito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
